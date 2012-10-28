3854d82a64d59adacb208da7c856d81907d17d83
Yura Nevsky <yura.nevsky@gmail.com>
2012-10-28 05:18:21 +0400

    deploy rc :)

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,16 @@
-from fabric.api import env, local, run, lcd
+from fabric.api import env, local, run, lcd, cd
+from fabric.contrib.files import exists
 
 env.project = '/var/www/againfm'
+
+
+"""
+починить рестарт редиса
+починить старт uwsgi
+права на /var/www/againfm
+(Permission denied: '/var/www/againfm/afm/static/.webassets-cache')
+virtualenv и поддержка откатов
+"""
 
 def vagrant():
     # change from the default user to 'vagrant'
@@ -28,3 +38,17 @@ def player():
 def handlebars():
     with lcd('afm/static/js'):
         local('handlebars templates/*.handlebars --min --output render.js')
+
+def setup():
+    run('aptitude install -y git-core vim-nox ruby1.9.1 ruby1.9.1-dev build-essential python-pip python-dev libevent-dev')
+    run('gem install chef --no-ri --no-rdoc')
+    if not exists('/var/www'):
+        run('mkdir /var/www')
+    if exists('/var/www/againfm'):
+        run('rm -rf /var/www/againfm')
+    with cd('/var/www'):
+        run('git clone https://github.com/outself/againfm.git')
+        with cd('againfm'):
+            run('pip install -r requirements.txt')
+            run('mkdir chef/tmp')
+            run('chef-solo -c /var/www/againfm/chef/solo.rb -j /var/www/againfm/chef/production.json')
