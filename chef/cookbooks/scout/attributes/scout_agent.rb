ee67be5bfe761bd553e4d5f9a9e4cd3af4c4e6ac
Yura Nevsky <yura.nevsky@gmail.com>
2012-10-31 20:24:12 +0400

    scout recipe

--- chef/cookbooks/scout/attributes/scout_agent.rb
+++ chef/cookbooks/scout/attributes/scout_agent.rb
@@ -1,4 +1,4 @@
-set_unless[:scout_agent][:user] = "scout"
-set_unless[:scout_agent][:group] = "scout"
-set_unless[:scout_agent][:version] = '5.0.3'
-set_unless[:scout_agent][:scout_bin]     = "/usr/bin/scout"
+set_unless[:scout_agent][:user] = 'scout'
+set_unless[:scout_agent][:group] = 'scout'
+set_unless[:scout_agent][:version] = '5.5.10'
+set_unless[:scout_agent][:scout_bin] = "/usr/local/bin/scout"
