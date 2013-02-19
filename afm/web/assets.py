8984747a2bad1f8078bc2ecc678ffc6b95f3df14
outself <outself@outself.ru>
2013-02-19 19:32:31 +0400

    optimize styles

--- afm/web/assets.py
+++ afm/web/assets.py
@@ -9,3 +9,7 @@ assets.register('scripts', Bundle(
     'js/swfobject.js',
     'js/app/radio.js',
     filters='uglifyjs', output='js/deploy/app.%(version)s.js'))
+
+assets.register('styles', Bundle(
+    'css/normalize.css',
+    'css/style.css', output='css/deploy/style.%(version)s.css'))
