f83c3f92f029de807e6e7102641dd3041d3374a4
outself <outself@outself.ru>
2013-02-20 20:32:30 +0400

    fix mail templates

--- afm/web/tasks.py
+++ afm/web/tasks.py
@@ -6,7 +6,8 @@ from afm import app
 def send_mail(email, body, subject=None):
     if subject is None:
         subject = app.config['DEFAULT_MAIL_SUBJECT']
-    return mailer.send_email(
+    result = mailer.send_email(
         source=app.config['DEFAULT_MAIL_SENDER'],
         subject=subject, body=body, to_addresses=[email], format='html',
     )
+    return result
