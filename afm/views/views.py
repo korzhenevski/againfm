031a7316c1620a2a2c554d94a846517b06dbadfc
outself <outself@outself.ru>
2013-04-25 18:55:59 +0400

    views

---
+++ afm/views/views.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import render_template, request
+from datetime import datetime
+
+from afm import db, app
+
+
+@app.route('/')
+@app.route('/signup')
+@app.route('/amnesia')
+@app.route('/login')
+def index():
+    return render_template('player.html')
+
+
+@app.route('/listen/<int:radio_id>')
+def listen(radio_id):
+    radio = db.Radio.find_one_or_404({'id': radio_id})
+    return render_template('player.html', radio=radio)
+
+
+@app.context_processor
+def app_context():
+    return {
+        'standalone': request.is_xhr,
+        'genres': [genre.get_public() for genre in db.RadioGenre.find({'is_public': True})],
+        'year': datetime.now().year,
+    }
