0f22f51c2504f740efe51f1aaba47a0bb0ff2b2f
outself <yura.nevsky@gmail.com>
2013-06-21 15:35:39 +0400

    remove old cookbooks

--- chef/cookbooks/timezone/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-file "/etc/timezone" do
-  content node.timezone.value
-end
-
-execute "Configure timezone" do
-  command "dpkg-reconfigure -f noninteractive tzdata"
-end
--- chef/solo.rb
+++
@@ -1,5 +0,0 @@
-root = File.absolute_path(File.dirname(__FILE__))
-
-file_cache_path root + '/tmp'
-cookbook_path root + '/cookbooks'
-role_path root + '/roles'
